6e10fa368509368a80177b6842aeed2676750741
Ashish Singh <ashking94@gmail.com>
2023-06-23 10:11:50 +0530

    Add new method for instrumentation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        App app = new App();
+        app.sayHelloAgain();
         System.out.println( "Hello World!" );
+    }
+
+    private void sayHelloAgain() {
+        System.out.println("Hello Again!!");
     }
 }
